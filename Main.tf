4dfcd654c36db0500192a6f5782b306bdc1c6df9
Ajitdevopsinsider <ajit1455chaurasia@gmail.com>
2025-07-11 00:34:43 +0530

    code added for conflict test

--- Main.tf
+++ Main.tf
@@ -4,6 +4,12 @@ module "rg" {
   
 }
 
+module "rg1" {
+  source = "../Module/ResourceGroup Module"
+  rg1    = var.rg
+  
+}
+
 # module "Storage_account" {
 #     depends_on = [ module.rg ]
 #     source = "../Module/Storage_Account_Module"
